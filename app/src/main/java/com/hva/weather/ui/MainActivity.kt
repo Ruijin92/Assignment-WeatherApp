7bcd335123c350bc4c7be15b6955496f7abeb2b8
Uray <uray92@hotmail.com>
2019-06-07 00:51:57 +0200

    Implemented functions for getting the premission

--- app/src/main/java/com/hva/weather/ui/MainActivity.kt
+++ app/src/main/java/com/hva/weather/ui/MainActivity.kt
@@ -1,17 +1,40 @@
 package com.hva.weather.ui
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationResult
 import com.hva.weather.R
 import kotlinx.android.synthetic.main.activity_main.*
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.closestKodein
+import org.kodein.di.generic.instance
 
-class MainActivity : AppCompatActivity() {
 
+private const val MY_PERMISSION_ACCESS_COARSE_LOCATION = 1
+
+class MainActivity : AppCompatActivity(), KodeinAware {
+
+    override val kodein by closestKodein()
+    private val fusedLocationProviderClient: FusedLocationProviderClient by instance()
     private lateinit var navController: NavController
+
+    //fixes the problem that the fusedLocationProver could be null
+    private val locationCallBack = object : LocationCallback(){
+        override fun onLocationResult(p0: LocationResult?) {
+            super.onLocationResult(p0)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +42,57 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(main_toolbar)
 
         setNavigationController()
+        requestLocationPermission()
+
+
+        if (hasLocationPermission()) {
+            bindLocationManager()
+        } else {
+            requestLocationPermission()
+        }
+    }
+
+    private fun bindLocationManager() {
+        LifecycleBoundLocationManager(
+            this,
+            fusedLocationProviderClient, locationCallBack
+        )
+    }
+
+    /**
+     * Request the permission from the user
+     */
+    private fun requestLocationPermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION),
+            MY_PERMISSION_ACCESS_COARSE_LOCATION
+        )
+    }
+
+    /**
+     * Checks in the manifest if the user already gave the permission for the location
+     */
+    private fun hasLocationPermission(): Boolean {
+        return ContextCompat.checkSelfPermission(this,
+            Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
+    }
+
+    /**
+     * If the user refuses to give the permission than a toast message is displayed. So the users knows that he
+     * has to set the location by himself to make it work
+     */
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == MY_PERMISSION_ACCESS_COARSE_LOCATION) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                bindLocationManager()
+            else
+                Toast.makeText(this, "Please, set location manually in settings", Toast.LENGTH_LONG).show()
+        }
     }
     /**
      * The NavigationController handles the transition between the different Fragments by looking up the IDs
